be94fcbe22fbe86ed3e12a91d1074364a56abba9
Mateo <mateo.j.ramallo@gmail.com>
2024-07-02 08:06:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Inicio} from "./components/Inicio";
+/* 
+pantalla inicial de nuestro proyecto
+*/
+
+function App() {
+  return (
+      <>
+          <Inicio/> {/* Se importa el inicio */}
+      </>
+  );
+  }
+  export default App;
+  
---
+++ src/components/Computadoras.jsx
@@ -0,0 +1,13 @@
+function Computadoras(){
+    return (
+        <>
+        <div className="tituloPagina">Computadoras Personales</div>
+        <div className="table-responsive">
+            <table className="table"></table>
+        </div>
+        </>
+    );
+}
+
+
+export { Computadoras };
